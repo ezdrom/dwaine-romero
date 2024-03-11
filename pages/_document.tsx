7dfa45ff55d2e1909f256f6ebcd8e50862cd5221
im your pc <ezdrom@gmail.com>
2024-03-11 22:40:09 +0800

    docs: changes to images and read me todos

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Next.js Blog Starter Kit",
-    description: "Clone and deploy your own Next.js portfolio in minutes.",
-    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+    title: "Ezequiel Dwaine Romero Blog & Portfolio",
+    description: "See Dwaine's world in Websites.",
+    image: "/images/utang.jpg",
   };
 
   return (
