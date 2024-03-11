3a8df8500cbcf0730d5695e0c5336a9eb008985d
im your pc <ezdrom@gmail.com>
2024-03-11 22:05:16 +0800

    feat: changes to about page

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require("gray-matter");
 
 async function generate() {
   const feed = new RSS({
-    title: "Your Name",
-    site_url: "https://yoursite.com",
-    feed_url: "https://yoursite.com/feed.xml",
+    title: "Ezequiel Dwaine Romero",
+    site_url: "https://ezequieldwaineromero.com",
+    feed_url: "https://ezequieldwaineromero.com/feed.xml",
   });
 
   const posts = await fs.readdir(path.join(__dirname, "..", "pages", "posts"));
